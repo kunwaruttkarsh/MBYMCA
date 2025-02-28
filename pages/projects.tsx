d39fc9704c31dacda4b492175b908527a9fdb4ad
kunwaruttkarsh <kunwaruttkarsh@gmail.com>
2025-02-28 16:31:13 +0530

    gutter

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -91,7 +91,7 @@ const Projects = () => {
           that he didn&apos;t exist.
         </p>
       </GlareCard>
-      <GlareCard className="flex flex-col items-start justify-end py-8 px-6" >
+      {/* <GlareCard className="flex flex-col items-start justify-end py-8 px-6" >
         <img
           className="h-full w-full absolute inset-0 object-cover"
           src="https://images.unsplash.com/photo-1512618831669-521d4b375f5d?q=80&w=3388&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -101,7 +101,7 @@ const Projects = () => {
           The greatest trick the devil ever pulled was to convince the world
           that he didn&apos;t exist.
         </p>
-      </GlareCard>
+      </GlareCard> */}
     </div>
   </div>
 
